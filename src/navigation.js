f39183bb8619f7ad8a37fb248bc4e75e2bb1d588
Hugo Persson <hugo.e.persson@gmail.com>
2023-09-23 21:59:10 +0200

    Started customizing the template for our needs and building good first landing page

--- src/navigation.js
+++ src/navigation.js
@@ -3,25 +3,17 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
+      text: 'Who are we',
+      href: '#about-us',
+    },
+
+    {
+      text: 'Our services',
+      href: '#services',
+    },
+    {
+      text: 'Earlier projects',
+      href: '#projects',
     },
     {
       text: 'Pages',
@@ -85,6 +77,7 @@ export const headerData = {
         },
       ],
     },
+
     {
       text: 'Blog',
       links: [
@@ -110,12 +103,8 @@ export const headerData = {
         },
       ],
     },
-    {
-      text: 'Widgets',
-      href: '#',
-    },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [{ text: 'Contact us', href: 'https://www.linkedin.com/in/hugo-persson-3ab782211/', target: '_blank' }],
 };
 
 export const footerData = {
